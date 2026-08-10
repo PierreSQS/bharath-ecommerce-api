c2b6d00a0bf65dfb8dab4bbd0650afa13051a435
PierreSQS <nomo_messina@web.de>
2026-08-10 15:53:55 +0200

    Sec3_Chap11: initial commit

---
+++ src/main/java/com/bharat/ecommerce/api/ecommerceapi/EcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.bharat.ecommerce.api.ecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApiApplication.class, args);
+    }
+
+}
